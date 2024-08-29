37fb36409cb01a35adf068bdff4171d246f6053c
Ryuu <2357622935@qq.com>
2024-08-29 17:17:24 +0800

    chore: clean project

--- src/org/ryuu/loop/exception/ArgumentException.lua
+++
@@ -1,6 +0,0 @@
-﻿local Exception = require "org.ryuu.loop.exception.Exception"
-local class = require "org.ryuu.loop.lang.keyword.class"
-
----@class ArgumentException:Exception
-local ArgumentException = class("ArgumentException", Exception)
-return ArgumentException
